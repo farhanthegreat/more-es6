d6a370ce753d4438fb56c4c4dafa92912ca5b61e
farhanthegreat <farhantuber@gmail.com>
2023-02-26 22:11:36 +0600

    nested object added

---
+++ js/chain.js
@@ -0,0 +1,9 @@
+const data = {
+    count: 5000,
+    data: [
+        {name: 'sandhanam', job: 'dealer'},
+        {name: 'rolex', job: 'maifa boss'}
+    ]
+}
+
+console.log(data.data[1].job);
--- js/recap.js
+++ js/recap.js
@@ -1,1 +1,20 @@
 const numbers = [12, 23, 43, 45, 42];
+
+//default parameter
+//I can give parameter a default value
+
+//template string
+//I can use backtick to code multiple line and can use ${} to add dynamic data
+
+//arrow function
+//I can declare a function by using arrow function
+// const Salary = (fixedSalary, tax, bonus) => fixedSalary - tax + bonus;
+
+//destructuring
+//I can get any data from object or array 
+//const {x,y,...c} = {x=12, y=13, z=14, a=5}
+
+//spread
+//I can get value from an array and can reassign as a new variable from any array.
+//const ages = [12,32,44,32,32];
+//const newAges = [...ages];
